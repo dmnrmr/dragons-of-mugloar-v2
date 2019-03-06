0f400cc0c3807e60b06e9b32368b3f3469c22942
Leonid Blinov <dmnrmr@gmail.com>
2019-03-06 23:15:24 +0200

    Add 404 page

---
+++ src/modules/NotFound/containers/NotFound.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="not-found">
+    <h1 class="not-found__title">
+      404
+    </h1>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+  .not-found {
+    display: flex;
+    height: 100%;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+
+    &__title {
+      font-size: 6em;
+      text-transform: uppercase;
+    }
+  }
+</style>
+
+<script>
+export default {
+  name: 'DmNotFound'
+};
+</script>
--- src/router.js
+++ src/router.js
@@ -3,6 +3,7 @@ import VueRouter from 'vue-router';
 import About from './modules/about/containers/About.vue';
 import Game from './modules/game/containers/Game.vue';
 import Home from './modules/home/containers/Home.vue';
+import NotFound from './modules/NotFound/containers/NotFound.vue';
 
 Vue.use(VueRouter);
 
@@ -10,7 +11,9 @@ const router = new VueRouter({
   routes: [
     { path: '/', component: Home },
     { path: '/about', component: About },
-    { path: '/play', component: Game }
+    { path: '/play', component: Game },
+    { path: '/404', component: NotFound },
+    { path: '*', redirect: '/404' }
   ]
 });
 
