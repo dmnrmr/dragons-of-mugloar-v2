479f93b562b56c4d71eb7ff569641df22450414b
Leonid Blinov <dmnrmr@gmail.com>
2019-03-23 12:32:14 +0200

    FEATURE: Style About page

--- src/views/pages/About/containers/About.vue
+++ src/views/pages/About/containers/About.vue
@@ -1,11 +1,30 @@
 <template>
-  <div>
-    <h1>About the game</h1>
-  </div>
+  <dm-content-layout>
+    <div class="content-about">
+      <h1 class="content-about__title">
+        About the game
+      </h1>
+    </div>
+  </dm-content-layout>
 </template>
 
 <style lang="scss" scoped>
-  h1 {
-    color: darkcyan;
+  .content-about {
+    padding: 1rem;
+
+    &__title {
+      text-transform: uppercase;
+    }
   }
 </style>
+
+<script>
+import DmContentLayout from '../../../layouts/ContentLayout.vue';
+
+export default {
+  name: 'DmHome',
+  components: {
+    DmContentLayout
+  }
+};
+</script>
